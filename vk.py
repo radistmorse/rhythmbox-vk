5d18c707a0e4b8dab0801692896c01feb4a77f8a
Radist Morse <radist.morse@gmail.com>
2013-03-29 00:55:41 +0100

    added copyright

--- vk.py
+++ vk.py
@@ -1,3 +1,19 @@
+# -*- coding: utf8 -*-
+# Copyright © 2013 Radist Morse <radist.morse@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from gi.repository import RB, Gio, Gtk, GdkPixbuf, GObject, Peas, PeasGtk
 import rb #"Loader" heler class
 from xml.dom import minidom #xml parser
